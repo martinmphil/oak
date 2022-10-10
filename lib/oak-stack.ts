0393e9a54dc9694888d6bf84b4e6e259717a080e
Martin <martin.mphil@gmail.com>
2022-10-10 09:45:15 +0100

    scaffolding candidate-email-address lambda fn

--- lib/oak-stack.ts
+++ lib/oak-stack.ts
@@ -1,16 +1,19 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as path from "path";
 
 export class OakStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'OakQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const canEmailAddrOak = new NodejsFunction(this, "canEmailAddrOak", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      // architecture: lambda.Architecture.ARM_64,
+      handler: "main",
+      entry: path.join(__dirname, `/../src/can-email-addr-oak/index.ts`),
+      bundling: { externalModules: ["aws-sdk"] },
+    });
   }
 }

df8d8bae88425f0227f7f4af8c4e0992271a5453
Martin <martin.mphil@gmail.com>
2022-11-07 14:06:44 +0000

    api-gateway http-api stack

--- lib/oak-stack.ts
+++ lib/oak-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from "constructs";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as path from "path";
+import { HttpApi, HttpMethod } from "@aws-cdk/aws-apigatewayv2-alpha";
+import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
 
 export class OakStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,10 +12,26 @@ export class OakStack extends cdk.Stack {
 
     const canEmailAddrOak = new NodejsFunction(this, "canEmailAddrOak", {
       runtime: lambda.Runtime.NODEJS_16_X,
-      // architecture: lambda.Architecture.ARM_64,
+      architecture: lambda.Architecture.ARM_64,
       handler: "main",
       entry: path.join(__dirname, `/../src/can-email-addr-oak/index.ts`),
       bundling: { externalModules: ["aws-sdk"] },
+    });
+    const canEmailAddrIntegration = new HttpLambdaIntegration(
+      "canEmailAddrIntegrationOak",
+      canEmailAddrOak
+    );
+
+    const gatewayOak = new HttpApi(this, "gatewayOak", {
+      // defaultAuthorizer: oakAuthorizor,
+      corsPreflight: {
+        allowHeaders: ["Authorization", "Content-Type"],
+      },
+    });
+    gatewayOak.addRoutes({
+      integration: canEmailAddrIntegration,
+      methods: [HttpMethod.GET],
+      path: "/email-addr",
     });
   }
 }

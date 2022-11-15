2d8a05a15cfc521ddf425eeb3412c67ef7b472ee
Martin <martin.mphil@gmail.com>
2022-11-15 13:03:32 +0000

    outline API, log group & demo lambda

--- lib/oak-stack.ts
+++ lib/oak-stack.ts
@@ -3,13 +3,19 @@ import { Construct } from "constructs";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as path from "path";
-import { HttpApi, HttpMethod } from "@aws-cdk/aws-apigatewayv2-alpha";
+import * as logs from "aws-cdk-lib/aws-logs";
+import {
+  HttpApi,
+  HttpMethod,
+  CorsHttpMethod,
+} from "@aws-cdk/aws-apigatewayv2-alpha";
 import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
 
 export class OakStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // lambda
     const canEmailAddrOak = new NodejsFunction(this, "canEmailAddrOak", {
       runtime: lambda.Runtime.NODEJS_16_X,
       architecture: lambda.Architecture.ARM_64,
@@ -17,15 +23,27 @@ export class OakStack extends cdk.Stack {
       entry: path.join(__dirname, `/../src/can-email-addr-oak/index.ts`),
       bundling: { externalModules: ["aws-sdk"] },
     });
+
+    new logs.LogGroup(this, "logGroupCanEmailAddrOak", {
+      logGroupName: `/aws/lambda/${canEmailAddrOak.functionName}`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
     const canEmailAddrIntegration = new HttpLambdaIntegration(
       "canEmailAddrIntegrationOak",
       canEmailAddrOak
     );
 
+    // gateway
     const gatewayOak = new HttpApi(this, "gatewayOak", {
       // defaultAuthorizer: oakAuthorizor,
       corsPreflight: {
-        allowHeaders: ["Authorization", "Content-Type"],
+        allowHeaders: ["Authorization"],
+        allowOrigins: [
+          "https://main.d3dq4xzxmmo3wf.amplifyapp.com/",
+          "http://localhost:6100",
+        ],
+        allowMethods: [CorsHttpMethod.GET],
       },
     });
     gatewayOak.addRoutes({

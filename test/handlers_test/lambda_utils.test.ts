6804a428673940ae6c2db51fdc3d4c4099eff5ca
Martin <martin.mphil@gmail.com>
2023-02-01 10:47:29 +0000

    lambda utility functions

---
+++ test/handlers_test/lambda_utils.test.ts
@@ -0,0 +1,60 @@
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+} from "@aws-sdk/lib-dynamodb";
+import { mockClient } from "aws-sdk-client-mock";
+import { getItem, putItem } from "../../lib/handlers/lambda_utils";
+
+describe("lambda utils", () => {
+  const originalEnv = process.env;
+  const dynamoMock = mockClient(DynamoDBDocumentClient);
+  beforeEach(() => {
+    jest.resetModules();
+    dynamoMock.reset();
+  });
+  afterEach(() => {
+    process.env = originalEnv;
+  });
+
+  it("exports get-item function", async () => {
+    dynamoMock
+      .on(GetCommand)
+      .resolves({
+        Item: undefined,
+      })
+      .on(GetCommand, {
+        TableName: "dummy_table_name",
+        Key: { pk: "standardCatalog", sk: "standardCatalog" },
+      })
+      .resolves({ Item: { catalog: ["exam1", "exam2"] } });
+
+    const Item = await getItem("standardCatalog", "standardCatalog");
+    expect(Item.catalog[0]).toStrictEqual("exam1");
+  });
+
+  it("exports put-item function", async () => {
+    dynamoMock
+      .on(PutCommand)
+      .resolves({})
+      .on(PutCommand, {
+        TableName: "dummy_table_name",
+        Item: { pk: "dummyPk", sk: "dummySk", newAttribute: "dummyValue" },
+      })
+      .resolves({
+        $metadata: {
+          httpStatusCode: 200,
+          requestId: "DUMMY1",
+          attempts: 1,
+          totalRetryDelay: 0,
+        },
+      });
+
+    const response = await putItem("dummyPk", "dummySk", {
+      newAttribute: "dummyValue",
+    });
+    expect(response["$metadata"]).toHaveProperty("httpStatusCode", 200);
+  });
+
+  //
+});

fe1e8c49213e248870f1123fb8b974a2efc6a6e4
Martin <martin.mphil@gmail.com>
2023-02-28 17:59:22 +0000

    candidate-email lambda function smoke test

--- lib/handlers/candidate-email/index.ts
+++ lib/handlers/candidate-email/index.ts
@@ -1,10 +1,20 @@
 // from @types
-import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import {
+  APIGatewayProxyEventV2WithJWTAuthorizer,
+  APIGatewayProxyResultV2,
+} from "aws-lambda";
+
+import {
+  CognitoIdentityProviderClient,
+  GetUserCommand,
+} from "@aws-sdk/client-cognito-identity-provider";
 
 export async function handler(
-  event: APIGatewayProxyEventV2
+  event: APIGatewayProxyEventV2WithJWTAuthorizer
 ): Promise<APIGatewayProxyResultV2> {
-  // console.log("event 👉", event);
+  // TEMP
+  console.log("event 👉", event);
+  //
   return {
     body: "You successfully invoked lambda candidate-email function from oak stack. ",
   };

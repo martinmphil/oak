529fd2e43c4d7702ea1949cf72cfd9fde3a07e71
Martin <martin.mphil@gmail.com>
2023-04-12 08:20:23 +0100

    lambda fn for candidate email address api endpoint

--- lib/handlers/candidate-email/index.ts
+++ lib/handlers/candidate-email/index.ts
@@ -1,21 +1,38 @@
 // from @types
-import {
-  APIGatewayProxyEventV2WithJWTAuthorizer,
-  APIGatewayProxyResultV2,
-} from "aws-lambda";
+import { APIGatewayProxyEventV2WithJWTAuthorizer } from "aws-lambda";
 
 import {
   CognitoIdentityProviderClient,
   GetUserCommand,
 } from "@aws-sdk/client-cognito-identity-provider";
 
-export async function handler(
-  event: APIGatewayProxyEventV2WithJWTAuthorizer
-): Promise<APIGatewayProxyResultV2> {
-  // TEMP
-  console.log("event 👉", event);
-  //
-  return {
-    body: "You successfully invoked lambda candidate-email function from oak stack. ",
-  };
+export async function handler(event: APIGatewayProxyEventV2WithJWTAuthorizer) {
+  try {
+    const AccessToken = event?.headers?.authorization;
+    const client = new CognitoIdentityProviderClient({ region: "eu-west-1" });
+    const body = await client
+      .send(new GetUserCommand({ AccessToken }))
+      .then((cogUser) => {
+        if (cogUser.UserAttributes) {
+          const email = cogUser.UserAttributes.find(
+            (x) => x.Name === "email"
+          )?.Value;
+
+          if (typeof email === "string" && email.length > 1) {
+            return email;
+          }
+        }
+        throw new Error(
+          " Our databank failed to find candidate email address. "
+        );
+      });
+
+    return { body };
+  } catch (err) {
+    let fault = ` The candidate-email lambda function failed. `;
+    if (err) {
+      fault += ` ${err.toString()} `;
+    }
+    return { fault };
+  }
 }

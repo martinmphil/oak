c2b655bb1e1334c1c6e70bbfaad612ee24365e1d
Martin <martin.mphil@gmail.com>
2023-04-14 07:08:36 +0100

    get standard catalog in cognito post confirm λ

--- lib/handlers/cognito-post-confirm/getStandardCatalog.ts
+++ lib/handlers/cognito-post-confirm/getStandardCatalog.ts
@@ -1,29 +1,13 @@
 import { getItem } from "../dynamoUtils";
+import { validCatalog } from "../validCatalog";
 
 export async function getStandardCatalog(): Promise<string[]> {
-  const defaultReturn = [""];
   const data = await getItem("standardCatalog", "standardCatalog");
   const standardCatalog = data?.catalog;
 
-  if (standardCatalog === undefined || standardCatalog === null) {
-    console.warn(` Database failed to supply catalog. `);
-    return defaultReturn;
-  }
-  if (Array.isArray(standardCatalog) === false) {
-    console.warn(` Database failed to supply standard catalog as an array. `);
-    return defaultReturn;
-  }
-  if (standardCatalog.length < 1) {
-    console.warn(` Database supplied standard catalog as an empty array. `);
-    return defaultReturn;
-  }
-
-  if (
-    Array.isArray(standardCatalog) === true &&
-    standardCatalog.every((el: any) => typeof el === "string" && el.length > 0)
-  ) {
+  if (validCatalog(standardCatalog)) {
     return standardCatalog;
   }
 
-  return defaultReturn;
+  return [""];
 }

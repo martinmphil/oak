915df54d25c6f7f15d77f81931be1e2a66667f82
Martin <martin.mphil@gmail.com>
2023-06-20 16:08:24 +0100

    workbook endpoint for achieved workflow results

--- lib/handlers/workbook/achievedWorkbook.ts
+++ lib/handlers/workbook/achievedWorkbook.ts
@@ -1,6 +1,27 @@
+import { formatAchieved } from "./formatAchieved";
+import { getAssessmentData } from "./getAssessmentData";
+import { gradeAssessmentData } from "./gradeAssessmentData";
+
 export async function achievedWorkbook(
   candidateId: string,
   workflowId: string
 ) {
-  return `This will be markup from achieved workbook(${candidateId}, ${workflowId})`;
+  try {
+    const assessmentData = await getAssessmentData(candidateId, workflowId);
+
+    if (assessmentData && assessmentData?.grade) {
+      return formatAchieved(assessmentData);
+    }
+
+    if (assessmentData) {
+      const graded = await gradeAssessmentData(assessmentData);
+      return formatAchieved(graded);
+    }
+
+    return formatAchieved();
+  } catch (err) {
+    throw new Error(
+      ` achievedWorkbook(${candidateId}, ${workflowId})) failed:- ${err} `
+    );
+  }
 }
--- test/handlers-test/workbook/achievedWorkbook.test.ts
+++ test/handlers-test/workbook/achievedWorkbook.test.ts
@@ -1,8 +1,33 @@
 import { achievedWorkbook } from "../../../lib/handlers/workbook/achievedWorkbook";
+import * as formatAchieveMod from "../../../lib/handlers/workbook/formatAchieved";
+import * as getAssessmentDataMod from "../../../lib/handlers/workbook/getAssessmentData";
+import * as gradeAssessmentDataMod from "../../../lib/handlers/workbook/gradeAssessmentData";
+import { dummyAchievedAssessmentData } from "./dummyAchievedAssessmentData";
+import { dummyUngradedAssessmentData } from "./dummyUngradedAssessmentData";
 
 describe("achievedWorkbook", () => {
   const candidateId = "candidate-dummy_username";
   const workflowId = "workflow101";
+
+  const getAssessmentDataSpy = jest.spyOn(
+    getAssessmentDataMod,
+    "getAssessmentData"
+  );
+
+  const formatAchieveSpy = jest.spyOn(formatAchieveMod, "formatAchieved");
+
+  const gradeAssessmentDataSpy = jest.spyOn(
+    gradeAssessmentDataMod,
+    "gradeAssessmentData"
+  );
+
+  beforeEach(() => {
+    getAssessmentDataSpy.mockResolvedValue(dummyUngradedAssessmentData);
+    gradeAssessmentDataSpy.mockResolvedValue(dummyAchievedAssessmentData);
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
   it("exists", () => {
     expect(achievedWorkbook).toBeDefined();
@@ -10,13 +35,31 @@ describe("achievedWorkbook", () => {
 
   it("returns a promise", () => {
     expect.assertions(1);
+    getAssessmentDataSpy.mockResolvedValue(dummyAchievedAssessmentData);
     expect(achievedWorkbook(candidateId, workflowId)).resolves.not.toThrow();
   });
 
-  it("resolves to a string", async () => {
-    expect.assertions(2);
+  it("can return default html", async () => {
+    expect.assertions(3);
+    getAssessmentDataSpy.mockResolvedValueOnce(undefined);
     const result = await achievedWorkbook(candidateId, workflowId);
     expect(typeof result).toBe("string");
     expect(result.length).toBeGreaterThan(1);
+    expect(result).toMatch(/achieved completion/i);
   });
+
+  it("it calls getAssessmentData", async () => {
+    expect.assertions(1);
+    await achievedWorkbook(candidateId, workflowId);
+    expect(getAssessmentDataSpy).toHaveBeenCalledWith(candidateId, workflowId);
+  });
+
+  it("calls formatAchieve via dummyAchievedAssessmentData", async () => {
+    expect.assertions(1);
+    getAssessmentDataSpy.mockResolvedValueOnce(dummyAchievedAssessmentData);
+    await achievedWorkbook(candidateId, workflowId);
+    expect(formatAchieveSpy).toBeCalledWith(dummyAchievedAssessmentData);
+  });
+
+  //
 });
